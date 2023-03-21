2e85c8a6ee95130ae8b8a6fa7b6a5b5025d8d0db
Hector Magana <42734419+Hecedu@users.noreply.github.com>
2023-03-20 19:35:22 -0600

    SEO improvements

--- jobmind/src/App.tsx
+++ jobmind/src/App.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Views/Home';
 
 function App() {
+
+  useEffect(() => {
+    document.title = 'JobMind AI';
+  }, []);
+
   return (
     <>
       <Routes>
