d3efd5d360b7ec2e0ebe4f150da9c2dca2c30511
Hector Magana <42734419+Hecedu@users.noreply.github.com>
2023-07-25 00:45:44 -0600

    Removed depressing icon

--- jobmind/src/Views/About.tsx
+++ jobmind/src/Views/About.tsx
@@ -4,10 +4,10 @@ export default function About() {
     return (
         <>
             <div className='border border-2 rounded-3 bg-black p-5'>
-                <h1 className='text-white display-1 text-center mb-3'>Experience the Next Generation of Business Tools</h1>
-                <h5 className='text-white text-center mb-3'>JobMind AI is dedicated to revolutionizing businesses and careers through the power of AI.</h5>
-                <p className='text-white text-center mb-3'>At JobMind AI, we are committed to creating AI business tools that enable our users to optimize their operations, increase efficiency, and drive growth. </p>
-                <p className='text-white fst-italic text-center'>Together, we can transform the way you work.</p>
+                <h1 className='text-white display-1 fw-bold mb-3'>Next Gen Business Tools</h1>
+                <h5 className='text-white mb-3'>JobMind AI is dedicated to revolutionizing businesses and careers through the power of AI.</h5>
+                <p className='text-white mb-3'>At JobMind AI, we are committed to creating business tools that enable our users to optimize their operations, increase efficiency, and drive growth. </p>
+                <p className='text-white fst-italic'>Let's transform the way you work.</p>
                 <div className='d-flex justify-content-center mb-3'>
                     <a href="mailto:hector.maganahdez@gmail.com" className="btn btn-dark">Contact Us</a>
                 </div>
--- jobmind/src/Views/Home.tsx
+++ jobmind/src/Views/Home.tsx
@@ -10,32 +10,40 @@ export default function Home() {
             <FadeIn transitionDuration={400} delay={200}>
                 <div className='bg-black p-5 w-100 rounded border border-2 border-white mt-5'>
                     <h1 className='display-1 fw-bold text-white text-start'>
-                        Elevate Your Career with AI Technology
+                        Business Focused AI Technology
                     </h1>
-                    <div className='d-flex justify-content-start mt-4'>
-                        <a href="https://www.producthunt.com/posts/jobmind-ai?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-jobmind&#0045;ai"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=393236&theme=light" alt="JobMind&#0032;AI - Elevate&#0032;your&#0032;career&#0032;with&#0032;AI&#0032;technology | Product Hunt" width="250" height="54" /></a>
-                    </div>
                 </div>
                 <div className='w-100 d-flex justify-content-center'>
                     <AiFillCaretDown className="text-white p-0 my-5 zoom" size={60} />
                 </div>
             </FadeIn>
 
-
             <CoveredCard />
+
+            <div className='w-100 bg-black border border-2 border-white rounded-3 p-5 mb-5'>
+                <h1 className='display-5 fw-bold text-center text-white'>
+                    SmartSlabs
+                </h1>
+                <h5 className='text-center text-white'>
+                    Coming Soon...
+                </h5>
+                <hr className='text-white'></hr>
+                <div className='d-flex justify-content-center'>
+                    <Link to={"/"} className='btn btn-dark border border-white border-2 shadow p-4 disabled'>Coming Soon</Link>
+                </div>
+            </div>
 
             <div className='w-100 bg-black border border-2 border-white rounded-3 p-5 mb-5'>
                 <h1 className='display-5 fw-bold text-center text-white'>
                     Intergenie
                 </h1>
                 <h5 className='text-center text-white'>
-                    Our next revolutionary tool
+                    Coming Soon...
                 </h5>
                 <hr className='text-white'></hr>
                 <div className='d-flex justify-content-center'>
                     <Link to={"/"} className='btn btn-dark border border-white border-2 shadow p-4 disabled'>Coming Soon</Link>
                 </div>
-
             </div>
         </>
     )
